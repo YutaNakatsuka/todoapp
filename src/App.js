26daf9cdfd896c723b574fe1f755bfe135ae841e
Nakatsun <98974553+YutaNakatsuka@users.noreply.github.com>
2022-09-20 00:22:10 +0900

    TODOアプリ完成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useState } from 'react';
 import './App.css';
+import { InputForm } from './compornents/InputForm';
+import { Title } from "./compornents/Title"
+import { TodoList } from './compornents/TodoList';
+
 
 function App() {
-  return <div className='App'></div>;
+  const [taskList, setTaskList] = useState([])
+
+  return <div className='body'>
+    <Title />
+    <InputForm taskList={taskList} setTaskList={setTaskList} />
+    <TodoList taskList={taskList} setTaskList={setTaskList} />
+  </div>;
 }
 
 export default App;
